bfceb812965b4fbecfbd090216f814d518c5c330
Maverick2797 <git.maverick2797@gmail.com>
2023-06-08 19:17:55 +0800

    First Module: Auto de-priv accounts on login
     - Provide a list of accounts and privs to reduce said accounts privs to
     - Used to prevent impact of account hijacking and impersonation of reserved accounts

---
+++ init.lua
@@ -0,0 +1,4 @@
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+
+-- Automatically de-priv certain admin accounts on join to minimise account hijacking
+dofile(modpath.."/admin_priv_deregister.lua")
